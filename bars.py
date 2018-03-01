c19cc098c8b3929f4003c52664a84683d7ff7297
Vladimir <smirnovv91@gmail.com>
2018-03-01 11:09:52 +0300

    fixed main func

--- bars.py
+++ bars.py
@@ -4,9 +4,9 @@ from geopy.distance import vincenty
 
 
 def load_data(filepath):
-    with open(filepath, "r", encoding="utf-8") as json_file:
-        decoded_json = json.load(json_file)
-    return decoded_json
+        with open(filepath, "r", encoding="utf-8") as json_file:
+            decoded_json = json.load(json_file)
+        return decoded_json
 
 
 def get_bars_data(decoded_json):
@@ -34,8 +34,8 @@ def get_closest_bar(bars_data, user_coordinates):
     closest_bar = min(
         bars_data, key=lambda x: vincenty(
             user_coordinates, (
-                x["geometry"]["coordinates"][0],
-                x["geometry"]["coordinates"][1]
+                    x["geometry"]["coordinates"][0],
+                    x["geometry"]["coordinates"][1]
             )
         ).km
     )
@@ -53,23 +53,23 @@ if __name__ == "__main__":
     try:
         file_path = sys.argv[1]
         bars_data = get_bars_data(load_data(file_path))
-    except FileNotFoundError:
-        exit("File not found")
-    except IndexError:
-        exit("Arguments error")
-    except ValueError:
-        exit("File is not a JSON")
-    print("Smallest bar: ", end="")
-    pprint_bar(get_smallest_bar(bars_data))
-    print("Biggest bar: ", end="")
-    pprint_bar(get_biggest_bar(bars_data))
-    try:
+        print("Smallest bar: ", end="")
+        pprint_bar(get_smallest_bar(bars_data))
+        print("Biggest bar: ", end="")
+        pprint_bar(get_biggest_bar(bars_data))
         longitude = float(input("Enter longitude:"))
         latitude = float(input("Enter latitude:"))
         user_coordinates = (longitude, latitude)
         closest_bar = get_closest_bar(bars_data, user_coordinates)
         print("Closest bar: {}".format(
-            closest_bar["properties"]["Attributes"]["Name"])
+            closest_bar["properties"]["Attributes"]["Name"]
+            )
         )
+    except FileNotFoundError:
+        exit("File not found")
+    except IndexError:
+        exit("Arguments error")
+    except json.decoder.JSONDecodeError:
+        exit("File not a JSON")
     except ValueError:
-        exit("Value Error")
+        exit("Value error")
